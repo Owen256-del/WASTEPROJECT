4ef263e9e6512f04b67e1de96813f3aa125f7b55
MUKWAYA OWEN <mukwayaowen21@gmail.com>
2026-04-29 16:05:09 +0100

    server fix

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ const PORT = 3000;
 
 const DATA_FILE = path.join(__dirname, "data.json");
 
-app.use(cors());
+app.use(cors({ origin: "https://smartecowaste.netlify.app/" }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
+// app.use(express.static(path.join(__dirname, "../public")));
 
 /**
  * Helper: Read data
